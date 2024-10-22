42e66ea6cb36e9fc47fea38921b8f2d153245f97
Torus-Ortie <velez.romain@orange.fr>
2024-10-22 15:17:10 +0200

    Implement Unit test

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -1,45 +1,126 @@
-import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { SessionService } from 'src/app/services/session.service';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+import { UserService } from '../../services/user.service';
 import { MeComponent } from './me.component';
+import { User } from "../../interfaces/user.interface";
+import { expect } from '@jest/globals';
+import { mockAdmin, mockUser } from '../../../test-constants';
 
 describe('MeComponent', () => {
   let component: MeComponent;
   let fixture: ComponentFixture<MeComponent>;
+  let userServiceMock: Partial<UserService>;
+  let sessionServiceMock: Partial<SessionService>;
+  let matSnackBarMock: Partial<MatSnackBar>;
+  let routerMock = { navigate: jest.fn() };
 
-  const mockSessionService = {
-    sessionInformation: {
-      admin: true,
-      id: 1
-    }
+  function setupUserServiceMock(user: User): Partial<UserService> {
+    return {
+      getById: jest.fn(() => of(user)),
+      delete: jest.fn(() => of(null)),
+    };
   }
-  beforeEach(async () => {
+
+  function setupSessionServiceMock(user: User): Partial<SessionService> {
+    return {
+      sessionInformation: {
+        id: user.id,
+        admin: user.admin,
+        token: 'mockToken',
+        type: 'mockType',
+        username: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName
+      },
+      logOut: jest.fn(() => of({})),
+      isLogged: true,
+    };
+  }
+
+  async function setupTestBed(user: User) {
+    userServiceMock = setupUserServiceMock(user);
+    sessionServiceMock = setupSessionServiceMock(user);
+    matSnackBarMock = {
+      open: jest.fn(),
+    };
+
     await TestBed.configureTestingModule({
       declarations: [MeComponent],
-      imports: [
-        MatSnackBarModule,
-        HttpClientModule,
-        MatCardModule,
-        MatFormFieldModule,
-        MatIconModule,
-        MatInputModule
+      imports: [MatSnackBarModule, MatCardModule, MatIconModule, RouterTestingModule],
+      providers: [
+        { provide: SessionService, useValue: sessionServiceMock },
+        { provide: UserService, useValue: userServiceMock },
+        { provide: MatSnackBar, useValue: matSnackBarMock },
+        { provide: Router, useValue: routerMock },
       ],
-      providers: [{ provide: SessionService, useValue: mockSessionService }],
-    })
-      .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  }
+
+  describe('when user is an admin', () => {
+    beforeEach(async () => {
+      await setupTestBed(mockAdmin);
+    });
+
+    it('should display the admin data', () => {
+      expect(component.user).toEqual(mockAdmin);
+    });
+
+    it('should not allow the admin to delete their account', () => {
+      const buttons = Array.from(fixture.nativeElement.querySelectorAll('button'));
+      const deleteButton = buttons.find(button => {
+        if (button instanceof HTMLButtonElement) {
+          return button.textContent?.includes('delete');
+        }
+        return false;
+      });
+      expect(deleteButton).toBeFalsy();
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('when user is a regular user', () => {
+    beforeEach(async () => {
+      await setupTestBed(mockUser);
+    });
+
+    it('should display the user data', () => {
+      expect(component.user).toEqual(mockUser);
+    });
+
+    it('should display the delete button', () => {
+      const buttons = Array.from(fixture.nativeElement.querySelectorAll('button'));
+      const deleteButton = buttons.find(button => {
+        if (button instanceof HTMLButtonElement) {
+          return button.textContent?.includes('delete');
+        }
+        return false;
+      });
+      expect(deleteButton).toBeTruthy();
+    });
+
+    it('should delete the user account and navigate to home when the delete button is clicked', (() => {
+      const snackBarSpy = jest.spyOn(matSnackBarMock, 'open');
+      component.delete();
+      expect(userServiceMock.delete).toHaveBeenCalledWith(mockUser.id.toString());
+      expect(snackBarSpy).toHaveBeenCalledWith("Your account has been deleted !", 'Close', { duration: 3000 });
+      expect(sessionServiceMock.logOut).toHaveBeenCalled();
+      expect(routerMock.navigate).toHaveBeenCalledWith(['/']);
+    }));
+  });
+
+  it('should navigate back when back button is clicked', () => {
+    const backSpy = jest.spyOn(window.history, 'back');
+    component.back()
+    expect(routerMock.navigate).toHaveBeenCalled();
+    expect(backSpy).toHaveBeenCalled();
   });
 });
--- front/src/app/features/sessions/services/session-api.service.spec.ts
+++ front/src/app/features/sessions/services/session-api.service.spec.ts
@@ -1,22 +1,94 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionApiService } from './session-api.service';
+import { Session } from '../interfaces/session.interface';
+import { mockSession } from "../../../../test-constants";
 
-describe('SessionsService', () => {
+describe('SessionApiService', () => {
   let service: SessionApiService;
+  let httpMock: HttpTestingController;
+  let session: Session;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports: [HttpClientTestingModule],
+      providers: [SessionApiService]
     });
+
     service = TestBed.inject(SessionApiService);
+    httpMock = TestBed.inject(HttpTestingController);
+    session = mockSession;
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should delete the session', () => {
+    const id = '1';
+
+    service.delete(id).subscribe();
+
+    const req = httpMock.expectOne(`api/session/${id}`);
+
+    expect(req.request.method).toBe('DELETE');
+
+    req.flush(null);
+  });
+
+  it('should create a session', () => {
+    service.create(session).subscribe();
+
+    const req = httpMock.expectOne('api/session');
+
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(session);
+
+    req.flush(session);
+  });
+
+  it('should update a session', () => {
+    const id = '1';
+  
+    service.update(id, session).subscribe();
+  
+    const req = httpMock.expectOne(`api/session/${id}`);
+  
+    expect(req.request.method).toBe('PUT');
+    expect(req.request.body).toEqual(session);
+  
+    req.flush(session);
+  });
+
+  it('should participate in a session', () => {
+    const sessionId = '1';
+    const userId = '1';
+  
+    service.participate(sessionId, userId).subscribe();
+
+    const req = httpMock.expectOne(`api/session/${sessionId}/participate/${userId}`);
+  
+    expect(req.request.method).toBe('POST');
+  
+    req.flush(null);
+  });
+
+  it('should unparticipate from a session', () => {
+    const sessionId = '1';
+    const userId = '1';
+
+    service.unParticipate(sessionId, userId).subscribe();
+
+    const req = httpMock.expectOne(`api/session/${sessionId}/participate/${userId}`);
+  
+    expect(req.request.method).toBe('DELETE');
+  
+    req.flush(null);
   });
 });
